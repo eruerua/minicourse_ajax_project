4f340c893e3522f31ee11e83d7df702d5a0a3239
eruera <eruerame@gmail.com>
2017-04-03 20:12:56 +0800

    add nytime and wiki api

--- js/script.js
+++ js/script.js
@@ -14,7 +14,47 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    var street = $('#street').val();
+    console.log(street);
+    var city = $('#city').val();
+    console.log(city);
+    var address = street + city;
+    var bgImagSource = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + address + ' ';
+    console.log(bgImagSource);
+    $body.append('<img class="bgimg" src="' + bgImagSource + '">');
 
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "5ada73e8ef20489a80e7d8fa27cd5750",
+      'q': city
+    });
+
+    $.getJSON(url, function(data) {
+        $nytHeaderElem.text('New York Times Articles About ' + city);
+        var articles = data.response.docs;
+        articles.forEach(function(article) {
+            $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+        });
+    }).error(function() {
+        $nytHeaderElem.text('New York Times Articles do not work');
+        });
+
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + city + '&format=json&callback=wikiCallback';
+
+    var wikiRequeatTimeout = setTimeout(function() {
+        $wikiElem.text("failed to get wikipedia resources");}, 8000);
+    $.ajax( {
+    url: wikiUrl,
+    dataType: 'jsonp',
+    success: function(data) {
+       // do something with data
+        var wikilist = data[1];
+        wikilist.forEach(function(article) {
+            $wikiElem.append('<li class="article">' + '<a href="http://en.wikipedia.org/wiki/' + article + '">' + article + '</a>' + '</li>');
+        });
+        clearTimeout(wikiRequeatTimeout);
+    }
+} );
     return false;
 };
 
